7a309881a9c8c63016a9824cb89e8a08e2d2aa5f
Mateus Benites Dias <imattdias@gmail.com>
2022-03-29 18:49:15 -0300

    CTD - Front End III - Checkpoint 1 - 2022-03-29

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,11 @@
-import foto from "../../assets/foto.jpg";
+import mateus from "../../assets/mateus.jpg";
 import "./styles.css";
 
 export default function About() {
     return (
         <main id="about">
-            <img src={foto} alt="Linguine" className="img-about" />
-            <p>Olá! Meu nome é Mateus, e atualmente sou aluno do Certified Tech Developer...</p>
+            <img src={mateus} alt="Mateus" className="img-about" />
+            <p>Olá! Meu nome é Mateus, sou advogado e Mestre em Direito, e atualmente estou cursando o 3º bimestre do Certified Tech Developer na Digital House Brasil. Esta página servirá como um repositório para os projetos em desenvolvimento ao longo da minha trajetória como aluno do CTD e como desenvolvedor de aplicações.</p>
         </main>
     );
 }
--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -1,21 +1,22 @@
-import { BsInstagram, BsLinkedin, BsGithub } from 'react-icons/bs';
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+import { FiMail } from "react-icons/fi";
 import "./styles.css";
 
 export default function Social() {
-    const instaUrl = "https://www.instagram.com/";
-    const linkedinUrl = "https://www.linkedin.com/";
-    const githubUrl = "https://www.github.com/";
+    const linkedinUrl = "https://www.linkedin.com/in/mateus-benites-dias/";
+    const githubUrl = "https://www.github.com/mateusbdias";
+    const mailtoUrl = "mailto:mateusdias@outlook.com";
     
     return (
         <div className="container-social">
-            <a href={instaUrl}>
-                <BsInstagram size={30} />
-            </a>
             <a href={linkedinUrl}>
                 <BsLinkedin size={30} />
             </a>
             <a href={githubUrl}>
                 <BsGithub size={30} />  
+            </a>
+            <a href={mailtoUrl}>
+                <FiMail size={30} />
             </a>
         </div>
     );
